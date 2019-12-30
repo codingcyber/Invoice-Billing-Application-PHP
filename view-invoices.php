0f07ae9c8f8d8c4ddd7dfcaf211e2620fe903279
codingcyber <vivek@codingcyber.com>
2019-12-30 14:02:55 +0530

    7-11 Display all the Invoices in View Invoices Page

--- view-invoices.php
+++ view-invoices.php
@@ -1,6 +1,10 @@
 <?php
     include('includes/header.php');
     include('includes/navigation.php');
+    require_once('includes/connect.php');
+    $sql = "SELECT * FROM invoices";
+    $result = $db->query($sql);
+    $res = $result->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <div id="page-wrapper" style="min-height: 345px;">
     <div class="row">
@@ -23,34 +27,30 @@
                             <thead>
                                 <tr>
                                     <th>#</th>
-                                    <th>Title</th>
-                                    <th>Order</th>
-                                    <th>Author</th>
+                                    <th>Client Name</th>
+                                    <th>Total</th>
+                                    <th>Payment Mode</th>
+                                    <th>Payment Reference</th>
                                     <th>Date</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                <?php foreach ($res as $invoice) { 
+                                    // fetch the client details from clients table
+                                    $clientsql = "SELECT * FROM clients WHERE id=?";
+                                    $clientresult = $db->prepare($clientsql);
+                                    $clientresult->execute(array($invoice['client_id']));
+                                    $clientres = $clientresult->fetch(PDO::FETCH_ASSOC);
+                                    ?>
                                 <tr>
-                                    <td>1</td>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    <td>@mdo</td>
+                                    <td><a href="view-single-invoice.php?id=<?php echo $invoice['id']; ?>"><?php echo $invoice['id']; ?></a></td>
+                                    <td><?php echo $clientres['name']; ?></td>
+                                    <td><?php echo $invoice['amount']; ?></td>
+                                    <td><?php echo $invoice['payment_mode']; ?></td>
+                                    <td><?php echo $invoice['payment_ref']; ?></td>
+                                    <td><?php echo $invoice['created']; ?></td>
                                 </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                    <td>@mdo</td>
-                                </tr>
+                                <?php } ?>
                             </tbody>
                         </table>
                     </div>
